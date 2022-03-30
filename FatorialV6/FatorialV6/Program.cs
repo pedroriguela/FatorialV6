8898b22435d8f516e31bf47895354318cff5bb92
Pedro Leone <origuela.pedro@escalate.com.br>
2022-03-30 12:12:02 -0300

    Subindo fatorial tentativa 6

---
+++ FatorialV6/FatorialV6/Program.cs
@@ -0,0 +1,43 @@
+﻿    namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número para ter seu fatorial: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Factorial(num));
+            Console.ReadLine();
+        }
+
+        public static int Fact(int num)
+        {
+            int resultado = 1;
+            
+            if ((num == 0) || (num ==1))
+            {
+                return 1;
+            }
+
+            for (int i = num; i > 0; i--)
+            {
+                resultado *= i;
+            }
+            return Console.WriteLine("O resultado é: ", resultado;
+        }
+
+        public static int Factorial(int num)
+        {
+            if ((num == 0) || (num == 1))
+            {
+                return 1;
+            }
+
+            return num * Factorial(num - 1);
+
+       
+        }
+
+    }
+}
