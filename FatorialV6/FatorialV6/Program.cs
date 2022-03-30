652836a73bc384c4e02c61220489fa8b9fb0541a
Pedro Leone <origuela.pedro@escalate.com.br>
2022-03-30 19:25:19 -0300

    Adicionado Stopwatch para medir tempo de execução.

--- FatorialV6/FatorialV6/Program.cs
+++ FatorialV6/FatorialV6/Program.cs
@@ -1,4 +1,8 @@
-﻿    namespace Fatorial
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Fatorial
 {
     class Program
     {
@@ -7,34 +11,41 @@
         {
             Console.WriteLine("Digite 1 para calcular usando Loop e 2 para Recursiva: ");
             int escolha = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
             int modeloCalc = 0;
-            
+
 
             Console.WriteLine("Digite um número para ter seu fatorial: ");
             int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            var stopWatch = Stopwatch.StartNew();
 
 
             if (escolha == 1)
             {
-                Console.WriteLine("modelo loop");
+                Console.WriteLine("O modelo escolhido foi Loop");
                 modeloCalc = Fact(num);
             }
 
             else if (escolha == 2)
             {
-                Console.WriteLine("modelo recursivo");
+                Console.WriteLine("O modelo escolhido foi Recursivo");
                 modeloCalc = Factorial(num);
             }
 
             Console.WriteLine("O fatorial de {0} é {1} usando o método {2}", num, modeloCalc, escolha);
+            stopWatch.Stop();
+            Console.WriteLine($"O tempo de execução foi de: {stopWatch.ElapsedMilliseconds}ms");
+
         }
-        
+
 
         public static int Fact(int num)
         {
             int resultado = 1;
-            
-            if ((num == 0) || (num ==1))
+
+            if ((num == 0) || (num == 1))
             {
                 return 1;
             }
@@ -44,20 +55,18 @@
                 resultado *= i;
             }
             return resultado;
-         
+
         }
 
         public static int Factorial(int num)
         {
-           
+
             if ((num == 0) || (num == 1))
             {
                 return 1;
             }
 
             return num * Factorial(num - 1);
-
-       
         }
     }
 }
